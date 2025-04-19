6942f4f2cde2b0c8444af6b3d712b293324424c7
Zwe Nay Lin <zwenaylin@Zwes-MacBook-Air.local>
2025-04-19 23:40:04 +0630

    Add milestone in FAQ

---
+++ app/faq/milestone.js
@@ -0,0 +1,59 @@
+import { BriefcaseIcon, GlobeAltIcon, RocketLaunchIcon, CurrencyDollarIcon } from "@heroicons/react/24/solid";
+
+export const milestones = [
+    {
+        year: "2012 Sept",
+        title: "Company Founded",
+        description: "We began our mission to innovate tech.",
+        icon: <RocketLaunchIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2016 Jan",
+        title: "System Launching",
+        description: "Launch of Central Bank of Myanmar Financial Network System (CBM-NET).",
+        icon: <BriefcaseIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2016 Feb",
+        title: "System Launching",
+        description: "MACCS/MCIS System Launch.",
+        icon: <CurrencyDollarIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2016 Nov",
+        title: "System Launching",
+        description: "MACCS/MCIS System start at Yangon Region.",
+        icon: <GlobeAltIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2018 Jun",
+        title: "Border Expansion",
+        description: "MACCS/MCIS Border Expansion at Myawaddy Region.",
+        icon: <GlobeAltIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2020 Nov",
+        title: "System Launching",
+        description: "Launch of Central Bank of Myanmar Financial Network System 2 (CBM-NET2), an advanced and upgraded version of original CBM-NET.",
+        icon: <GlobeAltIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2020 Dec",
+        title: "Hardware Renewal",
+        description: "MACCS/MCIS Hardware renewel.",
+        icon: <GlobeAltIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2022 Apr",
+        title: "Border Expansion",
+        description: "MACCS/MCIS Border Expansion at Muse Region.",
+        icon: <GlobeAltIcon className="w-6 h-6 text-blue-600" />
+      },
+      {
+        year: "2024 Apr",
+        title: "Global Expansion",
+        description: "BMACCS/MCIS Border Expansion at TachiLeik Region.",
+        icon: <GlobeAltIcon className="w-6 h-6 text-blue-600" />
+      },
+  ];
+  
